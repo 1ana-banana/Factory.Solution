5bcc62a4c0020ab0b7b09ef75e6a5f28f544da75
Anastasia Han <anastasia.han@yahoo.com>
2022-03-26 15:14:45 -0400

    Added Home View

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,49 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Engineer> engineerList = _db.Engineers.ToList();
+      engineerList.Sort((x, y) => string.Compare(x.LastName, y.LastName));
+      return View(engineerList);
+    }
+    public ActionResult Details (int id)
+    {
+      var thisEngineer = _db.Engineers
+        .Include(Engineer => Engineer.Machines)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(Engineer => Engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList (_db.Machines, "MachineId", "MachineName");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create (Engineer engineer, int MachineId)
+    {
+      _db.Engineers.Add(engineer);
+      if (MachineId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() {MachineId = MachineId, EngineerId = engineer.EngineerId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
